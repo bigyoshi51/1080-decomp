8b16354abd79c2440af24926392d1241aaaeb85c
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-04-18 12:17:10 -0400

    Decompile func_80004650 as __osPiRawStartDma

--- src/kernel/kernel_042.c
+++ src/kernel/kernel_042.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
+extern s32 D_80000308;
+extern s32 D_A4600000;
+extern s32 D_A4600004;
+extern s32 D_A4600008;
+extern s32 D_A460000C;
+extern s32 D_A4600010;
+extern s32 func_80004B30(s32);
 extern s32 func_800066B0(void);
 extern void func_800066D0(s32);
 extern s32 __osRdb_IP6_Data;
@@ -26,4 +33,28 @@ void func_800045C0(s32 addr, u32 size) {
     func_800066D0(saveMask);
 }
 
-INCLUDE_ASM("asm/nonmatchings/kernel", func_80004650);
+/* __osPiRawStartDma */
+s32 func_80004650(s32 direction, u32 devAddr, s32 dramAddr, u32 size) {
+    register u32 status;
+
+    status = D_A4600010;
+    while (status & 3) {
+        status = D_A4600010;
+    }
+
+    D_A4600000 = func_80004B30(dramAddr);
+    D_A4600004 = (D_80000308 | devAddr) & 0x1FFFFFFF;
+
+    switch (direction) {
+        case 0:
+            D_A460000C = size - 1;
+            break;
+        case 1:
+            D_A4600008 = size - 1;
+            break;
+        default:
+            return -1;
+    }
+
+    return 0;
+}
