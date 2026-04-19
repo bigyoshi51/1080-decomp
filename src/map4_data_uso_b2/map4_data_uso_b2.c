e697ccb281adf183cd5ad9ab9a2c7317c06b9784
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-04-19 17:58:13 -0400

    Decompile map4_data_uso_b2_func_00000000 (USO int reader (4 bytes))

--- src/map4_data_uso_b2/map4_data_uso_b2.c
+++ src/map4_data_uso_b2/map4_data_uso_b2.c
@@ -1,8 +1,20 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/map4_data_uso_b2/map4_data_uso_b2", map4_data_uso_b2_func_00000000);
+extern int gl_func_00000000();
+extern char D_00000000;
+typedef struct { int a, b, c, d; } Quad4;
 
-INCLUDE_ASM("asm/nonmatchings/map4_data_uso_b2/map4_data_uso_b2", map4_data_uso_b2_func_0000003C);
+void map4_data_uso_b2_func_00000000(int *dst) {
+    int buf[2];
+    gl_func_00000000(&D_00000000, buf, 4);
+    *dst = buf[0];
+}
+
+void map4_data_uso_b2_func_0000003C(Quad4 *dst) {
+    Quad4 buf;
+    gl_func_00000000(&D_00000000, &buf, 16);
+    *dst = buf;
+}
 
 INCLUDE_ASM("asm/nonmatchings/map4_data_uso_b2/map4_data_uso_b2", map4_data_uso_b2_func_00000094);
 
