cdd6b9f8be29e3e5c343b1226c3f37085436e622
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-04-19 18:07:53 -0400

    Decompile arcproc_uso_func_000009CC (USO Vec3 reader template)

--- src/arcproc_uso/arcproc_uso.c
+++ src/arcproc_uso/arcproc_uso.c
@@ -46,7 +46,20 @@ void arcproc_uso_func_00000974(Quad4 *dst) {
     *dst = buf;
 }
 
-INCLUDE_ASM("asm/nonmatchings/arcproc_uso/arcproc_uso", arcproc_uso_func_000009CC);
+typedef struct { int a, b, c; } Tri3i;
+typedef struct { float x, y, z; } Vec3;
+void arcproc_uso_func_000009CC(Vec3 *dst) {
+    int pad_top[1];
+    Tri3i raw;
+    int pad_mid[2];
+    Tri3i tmp;
+    int pad_bot[2];
+    gl_func_00000000(&D_00000000, &raw, 12);
+    tmp = raw;
+    dst->x = *(float*)&tmp.a;
+    dst->y = *(float*)&tmp.b;
+    dst->z = *(float*)&tmp.c;
+}
 
 INCLUDE_ASM("asm/nonmatchings/arcproc_uso/arcproc_uso", arcproc_uso_func_00000A3C);
 
