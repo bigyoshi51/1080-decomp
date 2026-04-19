392371e3fdec4ba1098f10caf0dc00afc96c93e2
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-04-19 17:58:10 -0400

    Decompile boarder5_uso_func_00000040 (USO Quad4 reader (16 bytes))

--- src/boarder5_uso/boarder5_uso.c
+++ src/boarder5_uso/boarder5_uso.c
@@ -1,14 +1,30 @@
 #include "common.h"
 
+extern int gl_func_00000000();
+extern char D_00000000;
+typedef struct { int a, b, c, d; } Quad4;
+
 INCLUDE_ASM("asm/nonmatchings/boarder5_uso/boarder5_uso", boarder5_uso_func_00000000);
 
-INCLUDE_ASM("asm/nonmatchings/boarder5_uso/boarder5_uso", boarder5_uso_func_00000040);
+void boarder5_uso_func_00000040(Quad4 *dst) {
+    Quad4 buf;
+    gl_func_00000000(&D_00000000, &buf, 16);
+    *dst = buf;
+}
 
 INCLUDE_ASM("asm/nonmatchings/boarder5_uso/boarder5_uso", boarder5_uso_func_00000098);
 
-INCLUDE_ASM("asm/nonmatchings/boarder5_uso/boarder5_uso", boarder5_uso_func_000000D4);
+void boarder5_uso_func_000000D4(int *dst) {
+    int buf[2];
+    gl_func_00000000(&D_00000000, buf, 4);
+    *dst = buf[0];
+}
 
-INCLUDE_ASM("asm/nonmatchings/boarder5_uso/boarder5_uso", boarder5_uso_func_00000110);
+void boarder5_uso_func_00000110(Quad4 *dst) {
+    Quad4 buf;
+    gl_func_00000000(&D_00000000, &buf, 16);
+    *dst = buf;
+}
 
 INCLUDE_ASM("asm/nonmatchings/boarder5_uso/boarder5_uso", boarder5_uso_func_00000168);
 
