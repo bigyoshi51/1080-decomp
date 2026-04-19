de82bdd5d6b00799f8faf5c7a672b55fb13dfc34
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-04-19 08:40:01 -0400

    Wrap gl_func_000671E4 as NON_MATCHING (a0 not saved to a3 before use)

--- src/game_libs/game_libs.c
+++ src/game_libs/game_libs.c
@@ -3188,7 +3188,18 @@ INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_00067168);
 
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_0006719C);
 
+#ifdef NON_MATCHING
+/* NON_MATCHING: expected saves a0 to a3 before using, mine uses a0 directly */
+extern int gl_func_00000000();
+extern char gl_ref_000416E0;
+void gl_func_000671E4(char *a0) {
+    *(int*)(a0 + 0x13E8) = 0;
+    *(int*)&gl_ref_000416E0 = 1;
+    gl_func_00000000(a0 + 0x11B0, &gl_ref_000416E0, 1);
+}
+#else
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_000671E4);
+#endif
 
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_00067220);
 
