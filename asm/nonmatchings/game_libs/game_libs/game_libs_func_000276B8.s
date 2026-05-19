cea0a7fee121f9ef2940cd30dcecd716cd5fb694
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-19 15:51:17 -0400

    1080 game_libs: split game_libs_func_000276B8 → game_libs_func_000276D0 (5 insns)

--- asm/nonmatchings/game_libs/game_libs/game_libs_func_000276B8.s
+++ asm/nonmatchings/game_libs/game_libs/game_libs_func_000276B8.s
@@ -1,4 +1,4 @@
-nonmatching game_libs_func_000276B8, 0x2C
+nonmatching game_libs_func_000276B8, 0x18
 
 glabel game_libs_func_000276B8
     /* E0C790 000276B8 8C880000 */  .word 0x8C880000
@@ -7,9 +7,4 @@ glabel game_libs_func_000276B8
     /* E0C79C 000276C4 8D250004 */  .word 0x8D250004
     /* E0C7A0 000276C8 03E00008 */  .word 0x03E00008
     /* E0C7A4 000276CC 00A61023 */  .word 0x00A61023
-    /* E0C7A8 000276D0 03E00008 */  .word 0x03E00008
-    /* E0C7AC 000276D4 00001025 */  .word 0x00001025
-    /* E0C7B0 000276D8 00001025 */  .word 0x00001025
-    /* E0C7B4 000276DC 03E00008 */  .word 0x03E00008
-    /* E0C7B8 000276E0 00000000 */  .word 0x00000000
 endlabel game_libs_func_000276B8
---
+++ asm/nonmatchings/game_libs/game_libs/game_libs_func_000276D0.s
@@ -0,0 +1,9 @@
+nonmatching game_libs_func_000276D0, 0x14
+
+glabel game_libs_func_000276D0
+    /* E0C7A8 000276D0 03E00008 */  .word 0x03E00008
+    /* E0C7AC 000276D4 00001025 */  .word 0x00001025
+    /* E0C7B0 000276D8 00001025 */  .word 0x00001025
+    /* E0C7B4 000276DC 03E00008 */  .word 0x03E00008
+    /* E0C7B8 000276E0 00000000 */  .word 0x00000000
+endlabel game_libs_func_000276D0
