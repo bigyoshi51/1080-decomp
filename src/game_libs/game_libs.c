3cf2be723f24fbf6afa20189911e2040df544ad1
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-19 15:23:12 -0400

    1080 game_libs: split game_libs_func_00009BE0 → game_libs_func_00009BF4 (113 insns)

--- src/game_libs/game_libs.c
+++ src/game_libs/game_libs.c
@@ -1490,6 +1490,8 @@ INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_00009BD8);
 
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_00009BE0);
 
+INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_00009BF4);
+
 /* Fragments 00037E98, 00037F10, 00037F40, 0003AA40, 0003AA5C, 00061F70
  * moved to game_libs_post.c (correct file for VRAM past 0x8944 cap of
  * game_libs.c). */
