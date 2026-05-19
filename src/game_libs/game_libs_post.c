67deab7a957e371866b0701be61f73824d6012a5
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-19 10:25:24 -0400

    1080 game_libs: split gl_func_0005DDE4 → game_libs_func_0005DF64 (51 insns)

--- src/game_libs/game_libs_post.c
+++ src/game_libs/game_libs_post.c
@@ -22996,6 +22996,8 @@ INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_0005DBB0);
 
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_0005DDE4);
 
+INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_0005DF64);
+
 #ifdef NON_MATCHING
 /* gl_func_0005E030: 33-insn 4x4 matrix add + dispatch (0x84, frame 0x70).
  *
