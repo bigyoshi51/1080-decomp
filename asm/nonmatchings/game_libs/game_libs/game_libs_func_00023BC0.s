8a665c9f36a1df9a61d58857876bfeeebd2b8da3
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-19 14:53:17 -0400

    1080 game_libs: split game_libs_func_00023BC0 → game_libs_func_00023BC8 (5 insns)

--- asm/nonmatchings/game_libs/game_libs/game_libs_func_00023BC0.s
+++ asm/nonmatchings/game_libs/game_libs/game_libs_func_00023BC0.s
@@ -1,11 +1,6 @@
-nonmatching game_libs_func_00023BC0, 0x1C
+nonmatching game_libs_func_00023BC0, 0x8
 
 glabel game_libs_func_00023BC0
     /* E08C98 00023BC0 03E00008 */  .word 0x03E00008
     /* E08C9C 00023BC4 8C62201C */  .word 0x8C62201C
-    /* E08CA0 00023BC8 03E00008 */  .word 0x03E00008
-    /* E08CA4 00023BCC 8C622020 */  .word 0x8C622020
-    /* E08CA8 00023BD0 8C632024 */  .word 0x8C632024
-    /* E08CAC 00023BD4 03E00008 */  .word 0x03E00008
-    /* E08CB0 00023BD8 00601025 */  .word 0x00601025
 endlabel game_libs_func_00023BC0
---
+++ asm/nonmatchings/game_libs/game_libs/game_libs_func_00023BC8.s
@@ -0,0 +1,9 @@
+nonmatching game_libs_func_00023BC8, 0x14
+
+glabel game_libs_func_00023BC8
+    /* E08CA0 00023BC8 03E00008 */  .word 0x03E00008
+    /* E08CA4 00023BCC 8C622020 */  .word 0x8C622020
+    /* E08CA8 00023BD0 8C632024 */  .word 0x8C632024
+    /* E08CAC 00023BD4 03E00008 */  .word 0x03E00008
+    /* E08CB0 00023BD8 00601025 */  .word 0x00601025
+endlabel game_libs_func_00023BC8
