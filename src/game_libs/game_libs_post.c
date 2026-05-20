6f4b426a4ab55af501441ce566584c75066bb0fc
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-20 03:25:24 -0400

    1080 game_libs: split game_libs_func_0006A1BC → game_libs_func_0006A1C4 (80 insns)

--- src/game_libs/game_libs_post.c
+++ src/game_libs/game_libs_post.c
@@ -32105,6 +32105,8 @@ INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_0006A144);
 
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_0006A1BC);
 
+INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_0006A1C4);
+
 #ifdef NON_MATCHING
 /* gl_func_0006A304: pointer-relocation fixup driver. Gets the fixed
  * object at segment offset 0x41710, calls the USO init/register cb
