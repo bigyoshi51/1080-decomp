2635d7db8b6d333408a44d4fbad124e7f6b051c2
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-20 01:45:20 -0400

    1080 game_libs: split game_libs_func_00060F64 → game_libs_func_00060F90 (183 insns)

--- src/game_libs/game_libs_post.c
+++ src/game_libs/game_libs_post.c
@@ -27620,6 +27620,8 @@ INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_00060F44);
 
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_00060F64);
 
+INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", game_libs_func_00060F90);
+
 INCLUDE_ASM("asm/nonmatchings/game_libs/game_libs", gl_func_0006126C);
 
 extern int gl_func_00000000();
