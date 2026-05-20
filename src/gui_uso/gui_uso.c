02f534243a0181ea9c7e3589a9f78f0298bd3a67
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-20 05:06:24 -0400

    1080 gui_uso: split gui_uso_func_000000B4 → gui_uso_func_000000CC (31 insns)

--- src/gui_uso/gui_uso.c
+++ src/gui_uso/gui_uso.c
@@ -82,6 +82,8 @@ INCLUDE_ASM("asm/nonmatchings/gui_uso/gui_uso", gui_uso_func_00000084);
 INCLUDE_ASM("asm/nonmatchings/gui_uso/gui_uso", gui_uso_func_0000009C);
 
 INCLUDE_ASM("asm/nonmatchings/gui_uso/gui_uso", gui_uso_func_000000B4);
+
+INCLUDE_ASM("asm/nonmatchings/gui_uso/gui_uso", gui_uso_func_000000CC);
 #endif
 
 /* gui_func_00000148: BUNDLED splat symbol (0x7D0 total / 500 insns).
