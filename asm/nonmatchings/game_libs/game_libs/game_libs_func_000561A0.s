7d9a3f1e383af211a7de45f59b54dc433f4dcef8
bigyoshi51 <bigyoshi51@users.noreply.github.com>
2026-05-22 15:31:43 -0400

    1080 game_libs: split game_libs_func_00056198 → game_libs_func_000561A0 (2 insns, LOW)

---
+++ asm/nonmatchings/game_libs/game_libs/game_libs_func_000561A0.s
@@ -0,0 +1,6 @@
+nonmatching game_libs_func_000561A0, 0x8
+
+glabel game_libs_func_000561A0
+    /* E3B278 000561A0 03E00008 */  .word 0x03E00008
+    /* E3B27C 000561A4 00000000 */  .word 0x00000000
+endlabel game_libs_func_000561A0
